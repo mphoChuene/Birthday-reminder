ae5594fb0f2009e3613174a6eb41a6803b3788c9
mphoChuene <mphochuene42@gmail.com>
2023-10-08 10:24:17 +0200

    completed birthday reminder

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, SafeAreaView, View, Text, TextInput, Button, FlatList, StyleSheet, Modal } from "react-native";
 import { name as appName } from "./app.json";
 import { Notifications } from "expo";
-import * as Permissions from "expo-permissions";
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  FlatList,
-  StyleSheet,
-} from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker"; // Import the date picker
 
 const App = () => {
   const [name, setName] = useState("");
   const [birthdate, setBirthdate] = useState("");
   const [reminders, setReminders] = useState([]);
+  const [isTodayBirthday, setIsTodayBirthday] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isDatePickerVisible, setDatePickerVisible] = useState(false); // State for showing/hiding the date picker
+
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
+
+  const handleDateConfirm = (date) => {
+    hideDatePicker();
+    const formattedDate = `${date.getMonth() + 1}/${date.getDate()}`;
+    setBirthdate(formattedDate);
+  };
 
   const addReminder = () => {
-    if (name && birthdate) {
+    if (name) {
       setReminders([...reminders, { name, birthdate }]);
       setName("");
       setBirthdate("");
+    } else {
+      alert("Please enter a name.");
     }
   };
 
@@ -33,53 +44,20 @@ const App = () => {
   );
 
   useEffect(() => {
-    // Request permissions for notifications (iOS and Android)
-    async function getPushNotificationPermissions() {
-      const { status: existingStatus } = await Permissions.getAsync(
-        Permissions.NOTIFICATIONS
-      );
-      if (existingStatus !== "granted") {
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        if (status !== "granted") {
-          alert("Please enable notifications for this app in settings.");
-          return;
-        }
-      }
+    // Check if today is the user's birthday
+    if (reminders.length > 0) {
+      const today = new Date();
+      const todayFormatted = `${today.getMonth() + 1}/${today.getDate()}`;
+      setIsTodayBirthday(reminders.some((reminder) => reminder.birthdate === todayFormatted));
     }
-
-    getPushNotificationPermissions();
-  }, []);
+  }, [reminders]);
 
   useEffect(() => {
-    // Schedule birthday reminders as notifications
-    reminders.forEach((reminder) => {
-      const { name, birthdate } = reminder;
-      const [month, day] = birthdate.split("/");
-      const now = new Date();
-      const notificationDate = new Date(
-        now.getFullYear(),
-        month - 1,
-        day,
-        9,
-        0,
-        0
-      ); // Set the time to 9:00 AM
-
-      if (notificationDate > now) {
-        const schedulingOptions = {
-          content: {
-            title: "Birthday Reminder",
-            body: `Today is ${name}'s birthday!`,
-          },
-          trigger: {
-            date: notificationDate,
-            repeats: true, // Repeat annually
-          },
-        };
-        Notifications.scheduleNotificationAsync(schedulingOptions);
-      }
-    });
-  }, [reminders]);
+    // Display a modal if today is the user's birthday
+    if (isTodayBirthday) {
+      setModalVisible(true);
+    }
+  }, [isTodayBirthday]);
 
   return (
     <View style={styles.container}>
@@ -95,15 +73,36 @@ const App = () => {
           style={styles.input}
           placeholder="Birthdate (MM/DD)"
           value={birthdate}
-          onChangeText={(text) => setBirthdate(text)}
+          onFocus={showDatePicker} // Show the date picker when the input is focused
         />
         <Button title="Add Reminder" onPress={addReminder} />
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleDateConfirm}
+          onCancel={hideDatePicker}
+        />
       </View>
       <FlatList
         data={reminders}
         keyExtractor={(item, index) => index.toString()}
         renderItem={renderReminderItem}
       />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>Today is your birthday!</Text>
+            <Button title="Close" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -118,6 +117,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 16,
+    marginTop: 48,
   },
   inputContainer: {
     marginBottom: 16,
@@ -138,6 +138,22 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 8,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    borderRadius: 8,
+    padding: 16,
+    width: "80%",
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 8,
+    textAlign: "center",
   },
 });
 
